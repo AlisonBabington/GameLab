74907f926b1b248d6ca1bc5591c976b39660d72e
Sharon Kynaston <desmogal@gmail.com>
2018-10-18 13:00:50 +0100

    enums created for potion, spell and weapon types

---
+++ src/main/java/game/tools/PotionType.java
@@ -0,0 +1,21 @@
+package game.tools;
+
+public enum  PotionType {
+
+    HEALPOTION1(40),
+    HEALPOTION2(30),
+    HEALPOTION3(20),
+    HEALPOTION4(10);
+
+    private final int value;
+
+    PotionType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+    public final int;
+
+}
---
+++ src/main/java/game/tools/SpellType.java
@@ -0,0 +1,20 @@
+package game.tools;
+
+public enum  SpellType {
+
+    ATTACK_SPELL(15),
+    DEFEND_SPELL(10);
+
+
+    private final int value;
+
+    SpellType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+    public final int
+
+}
